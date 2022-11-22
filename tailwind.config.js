3a5e521984a954d16253657749a3df6cbd9d6ef8
steven <alvaradosteven315@gmail.com>
2022-11-22 15:17:18 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        mWhite: '#ffffff',
+        mPurple: '#ea168e',
+        mBlack: '#000000',
+        mOverlay: '#5396de',
+        mGray: '#9a9a9a',
+      },
+    },
+  },
+  plugins: [],
+}
